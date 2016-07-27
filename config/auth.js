a1e3012ca088d9e6ff2cce65aa1c30c74c11194f
kumar <kumar.damani@mail.utoronto.ca>
2016-07-27 12:48:12 -0400

    facebook link updated

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID' : '1618318315164670',
         'clientSecret' : 'b4b67801b2b37f795871ce7cc55a33cf',
-        'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL' : 'http://frozen-springs-49303.herokuapp.com/auth/facebook/callback'
     }
 }

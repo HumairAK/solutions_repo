0a63131f12aec1540d60c0c9ef1ca9776f1d2855
kdam0 <kumar.damani@mail.utoronto.ca>
2017-01-02 13:11:32 -0500

    add confg vars support to use dev db

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use(function(error, req, res, next) {
 dbFile.setupDB(function (success, mssg) {
     if (success) {      // db establiseh
         server.listen(port, function(){         // now accept connections
-            console.log('listening on port 8080');
+            console.log('listening on port 3000');
         });
     }
     else {
